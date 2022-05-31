bd68c95fa1352539edb1e2224f6d827500b9d742
AkasHZamaN <akashzaman4@gmail.com>
2022-06-01 05:51:12 +0600

    modified

--- index.js
+++ index.js
@@ -43,6 +43,22 @@ async function run() {
     const orderCollection = client.db("superBycycle").collection("order");
     const userCollection = client.db("superBycycle").collection("users");
 
+
+
+    const verifyAdmin = async(req, res, next)=>{
+      const requester= req.decoded.email;
+      const requesterAccount = await userCollection.findOne({email: requester});
+      if(requesterAccount.role === 'admin'){
+          next();
+      }
+      else{
+          res.status(403).send({message: 'Forbiden'});
+      }
+  }
+
+
+
+
     //get all product from databse
     app.get("/product", async (req, res) => {
       const query = {};
@@ -59,11 +75,11 @@ async function run() {
       res.send(product);
     });
 
-    //delete order api get method
+    //delete  api get method
     app.delete("/product/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const product = await productCollection.findOne(query);
+      const product = await productCollection.deleteOne(query);
       res.send(product);
     });
 
@@ -118,29 +134,19 @@ async function run() {
 
 
 
-    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({email: requester});
-      if(requesterAccount.role === 'admin'){
-
-        const filter = { email: email };
+      const filter = { email: email };
       const updatedoc = {
         $set: {role: 'admin'},
       };
       const result = await userCollection.updateOne(filter, updatedoc);
       res.send(result);
-
-      }
-      else{
-        res.status(403).send({message: 'forbidden'});
-      }
-
     });
 
 
     //all user api
-    app.get('/user', async (req, res) => {
+    app.get('/user',verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
     });
